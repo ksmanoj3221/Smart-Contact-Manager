e544f239375587a8e7b4423e63752b34b0ac4c10
Manoj Kumar <ksmanoj3221@gmail.com>
2024-08-14 23:25:18 +0530

    Navbar: currentPage and nav

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -9,12 +9,14 @@ public class PageController {
     @RequestMapping("/home")
     public String home(Model model){
         model.addAttribute("name", "Manoj");
+        model.addAttribute("currentPage", "/home");
         return "home";
     }
 
     //about
     @RequestMapping("/about")
     public String aboutPage(Model model){
+        model.addAttribute("currentPage", "/about");
         model.addAttribute("isLoggedIn", true);
         return "about";
     }
@@ -22,6 +24,7 @@ public class PageController {
     //about
     @RequestMapping("/services")
     public String servicePage(Model model){
+        model.addAttribute("currentPage", "/services");
         return "services";
     }
     
