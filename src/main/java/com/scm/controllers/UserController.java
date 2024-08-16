113cc9a33bc3fb2a2687d34fddd2128ac3438f13
Manoj Kumar <ksmanoj3221@gmail.com>
2024-08-17 01:35:29 +0530

    Getting username (email)

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,34 +1,46 @@
-    package com.scm.controllers;
+package com.scm.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.scm.helpers.Helper;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    //user dashboard page
-    @RequestMapping(value = "/dashboard", method=RequestMethod.GET)
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    // user dashboard page
+    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String userDashboard() {
         return "user/dashboard";
     }
-    
-    //user profile page
-    @RequestMapping(value = "/profile", method=RequestMethod.GET)
-    public String userProfile() {
+
+    // user profile page
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public String userProfile(Authentication authentication) {
+        logger.info("Calling getEmailOfLoggedInUser");
+        String userName = Helper.getEmailOfLoggedInUser(authentication);
+        logger.info("User Logged in name : {}", userName);
         return "user/profile";
     }
-    
 
     // user add contact page
 
     // user view contacts
 
-    //user edit contact
+    // user edit contact
 
-    //user delete contact
+    // user delete contact
 
-    //user search contact
+    // user search contact
 }
