f52687263c74422851538956ad8d3c23553b2553
Manoj Kumar <ksmanoj3221@gmail.com>
2024-08-25 00:56:36 +0530

    Add contacts ui

--- src/main/java/com/scm/entities/Contact.java
+++ src/main/java/com/scm/entities/Contact.java
@@ -10,8 +10,17 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Contact {
 
     @Id
@@ -20,12 +29,12 @@ public class Contact {
     private String email;
     private String phoneNumber;
     private String picture;
-    
+
     @Column(length = 65535)
     private String description;
     private boolean favorite = false;
-    private String websiteLink; 
-    private String linkedInLink; 
+    private String websiteLink;
+    private String linkedInLink;
     // private List<String> socialLinks = new ArrayList<>();
 
     @ManyToOne
