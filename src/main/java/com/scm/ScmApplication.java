c7a5ddda8fadbbae34256b49e5ca8a16294d6966
Manoj Kumar <ksmanoj3221@gmail.com>
2024-08-31 20:50:17 +0530

    Mailtrap

--- src/main/java/com/scm/ScmApplication.java
+++ src/main/java/com/scm/ScmApplication.java
@@ -25,12 +25,13 @@ public class ScmApplication {
 		System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
 		System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
 		System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-		SpringApplication.run(ScmApplication.class, args);
 
 		// Mailtrap
 		System.setProperty("MAILTRAP_USERNAME", dotenv.get("MAILTRAP_USERNAME"));
 		System.setProperty("MAILTRAP_PASSWORD", dotenv.get("MAILTRAP_PASSWORD"));
+		System.setProperty("DOMAIN", dotenv.get("DOMAIN"));
 
+		SpringApplication.run(ScmApplication.class, args);
 	}
 
 }
--- src/test/java/com/scm/ScmApplicationTests.java
+++ src/test/java/com/scm/ScmApplicationTests.java
@@ -1,10 +1,17 @@
 package com.scm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.scm.services.EmailService;
 
 @SpringBootTest
 class ScmApplicationTests {
+
+	@Autowired
+	private EmailService service;
 
 	@Test
 	void contextLoads() {
